85e91467eb4febd2cd303d999b0d53a959a48935
Andrew William Seward <awswrd@gmail.com>
2016-10-16 12:35:35 -0700

    Add basic initial command parsing

--- src/Program.fs
+++ src/Program.fs
@@ -2,7 +2,39 @@
 
 open System
 
+type Command =
+  | List
+
+exception InvalidCommandException of string
+
+let parseCommand (command: string) =
+  match command.ToLowerInvariant() with
+  | "list" -> List
+  | _ -> raise (InvalidCommandException command)
+
+let getRemoteUrl localPath =
+  // TODO: Actually do this...
+  "https://github.com/awseward/ghi"
+
+let executeCommand args command =
+  match command with
+  | List ->
+    printfn "TODO: Implement list %A" args
+    1
+
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World!"
-    0 // return an integer exit code
+  match argv with
+  | [| |] ->
+    printfn "Usage: TODO"
+    1
+  | _ ->
+    try
+      argv
+      |> Seq.head
+      |> parseCommand
+      |> executeCommand (argv |> Array.toList |> List.tail)
+    with
+    | InvalidCommandException cmd ->
+        printfn "Error: Command `%s` does not exist" cmd
+        1
